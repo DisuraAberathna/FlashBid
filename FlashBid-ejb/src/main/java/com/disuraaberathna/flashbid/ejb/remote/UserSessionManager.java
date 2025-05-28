978302c2c4fe8a07c960d6fe492012aed359f502
Disura Aberathna <disura2005@gmail.com>
2025-05-28 19:34:56 +0530

    create UserSessionManagerBean interface

---
+++ FlashBid-ejb/src/main/java/com/disuraaberathna/flashbid/ejb/remote/UserSessionManager.java
@@ -0,0 +1,10 @@
+package com.disuraaberathna.flashbid.ejb.remote;
+
+import jakarta.ejb.Remote;
+
+@Remote
+public interface UserSessionManager {
+    String login(String username, String password);
+    String register(String username, String password);
+    void logout(String userId);
+}
