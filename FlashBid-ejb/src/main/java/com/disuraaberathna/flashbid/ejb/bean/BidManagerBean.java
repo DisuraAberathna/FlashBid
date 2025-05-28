a1523d22563b69db4571a16b10ddd8efa6813319
Disura Aberathna <disura2005@gmail.com>
2025-05-28 14:03:12 +0530

    cerate BidManagerBean

---
+++ FlashBid-ejb/src/main/java/com/disuraaberathna/flashbid/ejb/bean/BidManagerBean.java
@@ -0,0 +1,36 @@
+package com.disuraaberathna.flashbid.ejb.bean;
+
+import com.disuraaberathna.flashbid.core.dto.ResponseDto;
+import com.disuraaberathna.flashbid.core.model.AuctionItem;
+import com.disuraaberathna.flashbid.core.model.User;
+import com.disuraaberathna.flashbid.ejb.remote.BidManager;
+import com.disuraaberathna.flashbid.ejb.store.AuctionDataStore;
+import com.disuraaberathna.flashbid.ejb.validation.BidValidation;
+import com.google.gson.Gson;
+import jakarta.ejb.Stateless;
+import jakarta.inject.Inject;
+
+@Stateless
+public class BidManagerBean implements BidManager {
+
+    @Inject
+    private AuctionDataStore auctionDataStore;
+
+    @Inject
+    private BidValidation bidValidation;
+
+    @Override
+    public String placeBid(String auctionId, String bid, User user) {
+        Gson gson = new Gson();
+        ResponseDto responseDto = bidValidation.validateForUpdateBid(bid, auctionId);
+
+        if (responseDto.isSuccess()) {
+            AuctionItem auctionItem = auctionDataStore.getAuctionItem(auctionId);
+            auctionItem.setCurrentBid(bid);
+            auctionItem.setCurrentUser(user);
+            auctionDataStore.updateAuctionItem(auctionItem.getId(), auctionItem);
+        }
+
+        return gson.toJson(responseDto);
+    }
+}
