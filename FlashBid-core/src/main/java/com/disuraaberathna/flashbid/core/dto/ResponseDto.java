240d882194881b5ac403ffa7cddfe01f32c5a57e
Disura Aberathna <disura2005@gmail.com>
2025-05-28 14:01:38 +0530

    Create ResponseDto.java

---
+++ FlashBid-core/src/main/java/com/disuraaberathna/flashbid/core/dto/ResponseDto.java
@@ -0,0 +1,22 @@
+package com.disuraaberathna.flashbid.core.dto;
+
+public class ResponseDto {
+    private boolean success;
+    private String message;
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public void setSuccess(boolean success) {
+        this.success = success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
