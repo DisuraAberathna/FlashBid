5726fd7f90fc6150497ecb384e5b0bd1399b4fd7
Disura Aberathna <disura2005@gmail.com>
2025-05-28 19:59:48 +0530

    create user register

--- FlashBid-ejb/src/main/java/com/disuraaberathna/flashbid/ejb/bean/UserSessionManagerBean.java
+++ FlashBid-ejb/src/main/java/com/disuraaberathna/flashbid/ejb/bean/UserSessionManagerBean.java
@@ -1,15 +1,24 @@
 package com.disuraaberathna.flashbid.ejb.bean;
 
+import com.disuraaberathna.flashbid.core.dto.ResponseDto;
+import com.disuraaberathna.flashbid.core.model.User;
 import com.disuraaberathna.flashbid.ejb.remote.UserSessionManager;
 import com.disuraaberathna.flashbid.ejb.store.UserDataStore;
+import com.disuraaberathna.flashbid.ejb.validation.UserValidation;
+import com.google.gson.Gson;
 import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
+
+import java.util.UUID;
 
 @Stateless
 public class UserSessionManagerBean implements UserSessionManager {
 
     @Inject
     private UserDataStore userDataStore;
+
+    @Inject
+    private UserValidation userValidation;
 
     @Override
     public String login(String username, String password) {
@@ -18,7 +27,16 @@ public class UserSessionManagerBean implements UserSessionManager {
 
     @Override
     public String register(String username, String password) {
-        return "";
+        Gson gson = new Gson();
+        ResponseDto responseDto = userValidation.validateForRegister(username, password);
+
+        if (responseDto.isSuccess()) {
+            String id = UUID.randomUUID().toString();
+
+            userDataStore.addUser(new User(id, username, password));
+        }
+
+        return gson.toJson(responseDto);
     }
 
     @Override
