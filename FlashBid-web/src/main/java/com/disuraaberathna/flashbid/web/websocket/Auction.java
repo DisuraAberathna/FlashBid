f6a00b3e71823d07c2402fd776d48010b7fbdd22
Disura Aberathna <disura2005@gmail.com>
2025-05-31 15:28:22 +0530

    Update Auction.java
    update web socket connection

--- FlashBid-web/src/main/java/com/disuraaberathna/flashbid/web/websocket/Auction.java
+++ FlashBid-web/src/main/java/com/disuraaberathna/flashbid/web/websocket/Auction.java
@@ -7,23 +7,44 @@ import jakarta.websocket.Session;
 import jakarta.websocket.server.ServerEndpoint;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 @ServerEndpoint("/ws/auction")
 public class Auction {
+    protected static final List<Session> sessions = new ArrayList<>();
 
     @OnOpen
     public void onOpen(Session session) {
-        System.out.println("onOpen");
+        System.out.println("Connection opened: " + session.getId());
+        sessions.add(session);
     }
 
     @OnMessage
     public void onMessage(String message, Session session) throws IOException {
         System.out.println("Received: " + message);
-        session.getBasicRemote().sendText("Server echo: " + message);
     }
 
     @OnClose
     public void onClose(Session session) {
-        System.out.println("onClose");
+        System.out.println("Connection closed: " + session.getId());
+        sessions.remove(session);
+    }
+
+    public static void broadcast(String message) {
+        System.out.println("Broadcasting message: " + message);
+        System.out.println("Size of sessions: " + sessions.size());
+        for (Session session : sessions) {
+            if (session != null && session.isOpen()) {
+                try {
+                    session.getBasicRemote().sendText(message);
+                    System.out.println("Message sent to user : " + message);
+                } catch (Exception e) {
+                    System.out.println("Error sending message to user : " + e.getMessage());
+                }
+            } else {
+                System.out.println("User not found or session closed");
+            }
+        }
     }
 }
