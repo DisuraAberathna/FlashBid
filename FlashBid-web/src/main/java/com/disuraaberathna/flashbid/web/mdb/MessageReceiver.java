852b96927e69537c24bca1bde21aa3dfe47f0861
Disura Aberathna <disura2005@gmail.com>
2025-05-31 15:27:24 +0530

    Create MessageReceiver.java
    for receive messages from MDB

---
+++ FlashBid-web/src/main/java/com/disuraaberathna/flashbid/web/mdb/MessageReceiver.java
@@ -0,0 +1,25 @@
+package com.disuraaberathna.flashbid.web.mdb;
+
+import com.disuraaberathna.flashbid.web.websocket.Auction;
+import jakarta.ejb.ActivationConfigProperty;
+import jakarta.ejb.MessageDriven;
+import jakarta.jms.JMSException;
+import jakarta.jms.Message;
+import jakarta.jms.MessageListener;
+
+@MessageDriven(activationConfig = {
+        @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "jakarta.jms.Topic"),
+        @ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "activeMqTopic"),
+        @ActivationConfigProperty(propertyName = "destination", propertyValue = "activeMqTopic"),
+        @ActivationConfigProperty(propertyName = "resourceAdapter", propertyValue = "activemq-rar-6.1.6"),
+})
+public class MessageReceiver implements MessageListener {
+    @Override
+    public void onMessage(Message message) {
+        try {
+            Auction.broadcast(message.getBody(String.class));
+        } catch (JMSException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
