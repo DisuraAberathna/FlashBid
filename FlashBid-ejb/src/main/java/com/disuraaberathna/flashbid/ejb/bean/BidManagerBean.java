32192dde3cdab35bb61a149a7c85240c4215de09
Disura Aberathna <disura2005@gmail.com>
2025-05-31 15:26:48 +0530

    Update BidManagerBean.java
    call message sender when bid placed

--- FlashBid-ejb/src/main/java/com/disuraaberathna/flashbid/ejb/bean/BidManagerBean.java
+++ FlashBid-ejb/src/main/java/com/disuraaberathna/flashbid/ejb/bean/BidManagerBean.java
@@ -2,12 +2,13 @@ package com.disuraaberathna.flashbid.ejb.bean;
 
 import com.disuraaberathna.flashbid.core.dto.ResponseDto;
 import com.disuraaberathna.flashbid.core.model.AuctionItem;
-import com.disuraaberathna.flashbid.core.model.User;
 import com.disuraaberathna.flashbid.ejb.remote.BidManager;
+import com.disuraaberathna.flashbid.ejb.remote.MessageSendManager;
 import com.disuraaberathna.flashbid.ejb.store.AuctionDataStore;
 import com.disuraaberathna.flashbid.ejb.store.UserDataStore;
 import com.disuraaberathna.flashbid.ejb.validation.BidValidation;
 import com.google.gson.Gson;
+import jakarta.ejb.EJB;
 import jakarta.ejb.Stateless;
 import jakarta.inject.Inject;
 
@@ -23,6 +24,9 @@ public class BidManagerBean implements BidManager {
     @Inject
     private BidValidation bidValidation;
 
+    @EJB
+    private MessageSendManager messageSendManager;
+
     @Override
     public String placeBid(String auctionId, String bid, String userid) {
         Gson gson = new Gson();
@@ -33,6 +37,8 @@ public class BidManagerBean implements BidManager {
             auctionItem.setCurrentBid(bid);
             auctionItem.setCurrentUser(userDataStore.getUser(userid));
             auctionDataStore.updateAuctionItem(auctionItem.getId(), auctionItem);
+
+            messageSendManager.sendMessage(auctionId);
         }
 
         return gson.toJson(responseDto);
